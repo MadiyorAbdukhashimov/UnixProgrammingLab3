7c6e3b441a0c2f41bd0fb2267a3bd194b0a5c793
Max <tashkent.1997.1.7@gmail.com>
2019-03-01 17:50:43 +0500

    TASK 2

--- l3test2.c
+++ l3test2.c
@@ -7,31 +7,22 @@
 #include <stdio.h>
 #include <string.h>
 
-// U1610131 - @author - Madiyor Abdukhashimov
-// UNIX LAB ASSIGMENT NUMBER 3
-// this program takes input from argument
-
-
 int main(int argc, char const *argv[])
 {
-    if (argc < 2) {
-        printf("Please enter as ./exe pathName\n");
-        exit(1);
-    }
-    
     DIR *dp = opendir("./"); // we are just opening the current directory
     struct dirent *dir;
     struct stat st;
+
     char *pathName;
     pathName = (char *)(malloc(sizeof(char)*strlen(argv[1])));
     chdir(pathName);
     while((dir = readdir(dp)) != NULL){
-        if (stat(dir->d_name, &st)) {
+        if (lstat(dir->d_name, &st)) {
             printf("error in: stat(%s, &n)\n", dir->d_name);
             continue;
         }
-        if(S_ISREG(st.st_mode)){
-            printf(":::Regular File Found::: %s\n", dir->d_name);
+        if(S_ISLNK(st.st_mode)){
+            printf(":::Link File Found::: %s\n", dir->d_name);
         }
     }
     closedir(dp);
